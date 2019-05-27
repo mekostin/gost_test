e6e111e4df8c3ab493ffdc289b5f69956a8f36f2
Mikhail Kostin <me.kostin@gmail.com>
2019-05-27 17:24:46 +0300

    gost_cfb

--- gost.cpp
+++ gost.cpp
@@ -11,6 +11,9 @@ unsigned char	uKey[32]={0};
 unsigned char in[16]={0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
 unsigned char out[16]={0};
 
+unsigned char iv_enc[16]={0};
+unsigned char iv_dec[16]={0};
+
 void dump(const char *caption, unsigned char *buffer, int size) {
   printf("%s\n", caption);
   for (int i=0; i<size; i++) {
@@ -22,6 +25,10 @@ void dump(const char *caption, unsigned char *buffer, int size) {
 int main() {
   scanf("%32s", uKey);
   dump("KEY: ", uKey, 32);
+
+
+  memset(iv_enc, 0, sizeof(iv_enc));
+  memset(iv_dec, 0, sizeof(iv_dec));
 
   gost_init(&ekey, &GostR3411_94_TestParamSet);
   gost_key(&ekey, uKey);
@@ -39,6 +46,16 @@ int main() {
   memset(in, 0, sizeof(in));
   gost_dec(&dkey, out, in, sizeof(out)/8);
   dump("DECRYPTED: ", in, sizeof(in));
+
+
+  memset(out, 0, sizeof(out));
+  gost_enc_cfb(&ekey, iv_enc, in, out, sizeof(in)/8);
+  dump("ECRYPTED CFB: ", out, sizeof(out));
+
+  memset(in, 0, sizeof(in));
+  gost_dec_cfb(&dkey, iv_dec, out, in, sizeof(out)/8);
+  dump("DECRYPTED CFB: ", in, sizeof(in));
+
 
   return 0;
 }
