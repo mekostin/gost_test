c9fcfb3b3d55921000603e47ec7bbbbc33d6d355
Mikhail Kostin <me.kostin@gmail.com>
2019-05-27 13:46:31 +0300

    gost test

---
+++ Makefile
@@ -0,0 +1,32 @@
+PROG					= gost_test
+SOURCES				= gost gost89
+
+DIROBJ    		= obj/
+FILEOBJ     	= $(addsuffix .o, $(SOURCES))
+PATHOBJ     	= $(addprefix $(DIROBJ), $(FILEOBJ))
+CFLAGS				= -static -std=gnu90 -O2
+CC						= gcc
+
+VPATH = $(DIRSOUR) $(DIROBJ)
+
+.PHONY: all clean objdir
+
+all: objdir Makefile $(PROG)
+
+objdir:
+	@rm -Rf $(DIROBJ)
+	@mkdir $(DIROBJ)
+
+$(PROG): $(FILEOBJ)
+	$(CC) $(CFLAGS) -o $(PROG) $(addprefix $(DIROBJ), $^)
+
+%.o: %.cpp
+	$(CC) $(CFLAGS) -c $< -o $(DIROBJ)$@
+
+clean:
+	@rm -Rf $(PROG)
+	@rm -Rf $(DIROBJ)
+	@rm -Rf core*
+
+test:
+	@openssl enc -aes-256-ecb -k secret -P | grep key | awk '{split($$0, arr, "="); print arr[2]}' | ./$(PROG)
---
+++ gost.cpp
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "gost89.h"
+
+gost_ctx ekey;
+gost_ctx dkey;
+
+unsigned char	uKey[32]={0};
+
+unsigned char in[16]={0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
+unsigned char out[16]={0};
+
+void dump(const char *caption, unsigned char *buffer, int size) {
+  printf("%s\n", caption);
+  for (int i=0; i<size; i++) {
+    printf("%02X ", buffer[i]);
+  }
+  printf("\n");
+}
+
+int main() {
+  scanf("%32s", uKey);
+  dump("KEY: ", uKey, 32);
+
+  gost_init(&ekey, &GostR3411_94_TestParamSet);
+  gost_key(&ekey, uKey);
+
+  gost_init(&dkey, &GostR3411_94_TestParamSet);
+  gost_key(&dkey, uKey);
+
+  dump("MSG: ", in, sizeof(in));
+
+  memset(out, 0, sizeof(out));
+  gostcrypt(&ekey, in, out);
+
+  dump("ECRYPTED: ", out, sizeof(out));
+
+  memset(in, 0, sizeof(in));
+  gostdecrypt(&dkey, out, in);
+  dump("DECRYPTED: ", in, sizeof(in));
+
+  return 0;
+}
